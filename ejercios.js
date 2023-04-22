fbd9bf1487524acebb679b748c874bdb3bccb7fc
amelia588 <1473967@senati.pe>
2023-04-22 10:57:50 -0500

    fechas

--- ejercios.js
+++ ejercios.js
@@ -1,8 +1,12 @@
-const event=new Date('August 19, 1975 23:15:30');
-Event.setDate(24);
-console.log(Event.getDate());
-//expected output:24
-Event.setDate(32);
-//only 31 days in august
-console.log(Event.getDate());
-//expected output:1
+const fecha = new Date();
+const fecha = new Date('Jul 12 2011');
+const añoActual = fecha.getFullYear();
+console.log(añoActual); //2020
+const hoy = fecha.getDate();
+const mesActual = fecha.getMonth() + 1; 
+const tiempoTranscurrido = Date.now();
+const hoy = new Date(tempoTranscurrido);
+hoy.toDateString(); // "Sun Jun 14 2020"
+hoy.toISOString(); // "2020-06-13T18:30:00.000Z"
+hoy.toLocaleDateString(); // "14/6/2020"
+
